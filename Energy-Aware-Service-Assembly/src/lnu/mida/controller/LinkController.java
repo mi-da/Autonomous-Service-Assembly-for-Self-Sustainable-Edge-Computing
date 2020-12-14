01a62b035b1f2283e0a0867bf278765d4a846bc2
francescaricci10 <40519199+francescaricci10@users.noreply.github.com>
2020-12-14 11:54:41 +0100

    Update LinkController.java

--- Energy-Aware-Service-Assembly/src/lnu/mida/controller/LinkController.java
+++ Energy-Aware-Service-Assembly/src/lnu/mida/controller/LinkController.java
@@ -48,9 +48,7 @@ public class LinkController implements Control {
 	
 	
 	public boolean execute() {
-	
-		System.out.println("\n\n----- link controller ---- \n ");
-		
+			
 		for (int i = 0; i < Network.size(); i++) {
 
 			if (!Network.get(i).isUp()) {
@@ -60,14 +58,11 @@ public class LinkController implements Control {
 			GeneralNode node = (GeneralNode) Network.get(i);
 			OverloadComponentAssembly ca = (OverloadComponentAssembly) node.getProtocol(component_assembly_pid);
 			OverloadApplication appl = (OverloadApplication) node.getProtocol(application_pid);
-			
-			ca.printCandidates();
-			
+						
 			ArrayList<Service> services = ca.getServices();
 			ArrayList<ArrayList<Service>> candidates = ca.getCandidates();
 			
 			for (Service service : services) {
-				System.out.println("-- considero Service : " + service.getService_id());
 				
 				// per ogni dipendenza di ogni servizio cerco tra i possibili candidati
 				Service[] listDepObj = service.getDependencies_obj();
@@ -77,28 +72,19 @@ public class LinkController implements Control {
 					
 					boolean dep = listDep[j];
 					if (dep == true) {
-						//System.out.println("	dep di tipo : " + j);
 						// si selezionano i candidati di tipo j
 						ArrayList<Service> candidates_of_type = new ArrayList<>();
 						int index = (int) service.getService_id()%services_per_node;
 
-						//System.out.println("		candidates_of_type : " );
 						for(int k=0; k<candidates.get(index).size();k++) {
 							if(candidates.get(index).get(k).getType()==j) {
 								candidates_of_type.add(candidates.get(index).get(k));
-								//System.out.println("				" + candidates.get(count).get(k).getService_id());
 							}
-						}
-						
-						System.out.println("\n candidates of type : " + j);
-						for(int l=0; l<candidates_of_type.size(); l++) {
-							System.out.println(candidates_of_type.get(l).getService_id());
 						}
 						
 						Service to_link = appl.chooseByStrategy2(candidates_of_type);
 						if(to_link==null)
 							return false;
-						System.out.println("				\nServizio scelto : " + to_link.getService_id() + "\n\n");
 
 						Service depObj = listDepObj[j];
 						
