7f515439558aa4d97180ae992986c5c25329edac
francescaricci10 <40519199+francescaricci10@users.noreply.github.com>
2020-12-17 11:23:16 +0100

    Update LinkController.java

--- Energy-Aware-Service-Assembly/src/lnu/mida/controller/LinkController.java
+++ Energy-Aware-Service-Assembly/src/lnu/mida/controller/LinkController.java
@@ -2,8 +2,10 @@ package lnu.mida.controller;
 
 import java.util.ArrayList;
 
+import lnu.mida.entity.CandidateServices;
 import lnu.mida.entity.GeneralNode;
 import lnu.mida.entity.Service;
+import lnu.mida.entity.StrategyHandler;
 import lnu.mida.protocol.OverloadApplication;
 import lnu.mida.protocol.OverloadComponentAssembly;
 import peersim.config.Configuration;
@@ -14,11 +16,8 @@ import peersim.core.Network;
 
 public class LinkController implements Control {
 	
-	
-	
-	
 	/**
-	 * The component assambly protocol.
+	 * The component assembly protocol.
 	 */
 	private static final String COMP_PROT = "comp_prot";
 
@@ -35,9 +34,7 @@ public class LinkController implements Control {
 
 	private int services_per_node;
 
-	
-	
-	
+		
 	public LinkController(String name) {
 		this.name = name;
 		component_assembly_pid = Configuration.getPid(name + "." + COMP_PROT);
@@ -45,10 +42,9 @@ public class LinkController implements Control {
 		services_per_node = Configuration.getInt("SERVICES_PER_NODE", 0);
 	}
 	
-	
-	
+		
 	public boolean execute() {
-			
+					
 		for (int i = 0; i < Network.size(); i++) {
 
 			if (!Network.get(i).isUp()) {
@@ -60,11 +56,12 @@ public class LinkController implements Control {
 			OverloadApplication appl = (OverloadApplication) node.getProtocol(application_pid);
 						
 			ArrayList<Service> services = ca.getServices();
-			ArrayList<ArrayList<Service>> candidates = ca.getCandidates();
+			CandidateServices candidates = ca.getCandidateServices();
+			
+			StrategyHandler handler = new StrategyHandler();
 			
 			for (Service service : services) {
-				
-				// per ogni dipendenza di ogni servizio cerco tra i possibili candidati
+	
 				Service[] listDepObj = service.getDependencies_obj();
 				boolean[] listDep = service.getDependencies();
 				
@@ -72,38 +69,23 @@ public class LinkController implements Control {
 					
 					boolean dep = listDep[j];
 					if (dep == true) {
-						// si selezionano i candidati di tipo j
-						ArrayList<Service> candidates_of_type = new ArrayList<>();
-						int index = (int) service.getService_id()%services_per_node;
-
-						// refactor with new data structure
-						for(int k=0; k<candidates.get(index).size();k++) {
-							if(candidates.get(index).get(k).getType()==j) {
-								candidates_of_type.add(candidates.get(index).get(k));
-							}
-						}
 						
-						Service to_link = appl.chooseByStrategy2(candidates_of_type);
+						if(candidates.getCandidateServices(j).isEmpty())
+							continue;
+						
+						Service to_link = handler.chooseByStrategy(candidates.getCandidateServices(j), node);
 						if(to_link==null)
 							return false;
 
-						Service depObj = listDepObj[j];
-						
-						if(depObj==null) {
-							service.linkDependency(to_link);
-						}else {
-							service.unlinkDependency(depObj);
-							service.linkDependency(to_link);
-						}
+						service.addLink(to_link);						
 					}
 				}
+				
+				service.updateCompoundUtility();
 			}
 		}
-		
-		
-		
+	
 		return false;
 	}
 	
-
 }
