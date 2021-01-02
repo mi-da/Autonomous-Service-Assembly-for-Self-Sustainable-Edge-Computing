926889fb112d70a6d51d252a519bb1e51f664ccd
francescaricci10 <40519199+francescaricci10@users.noreply.github.com>
2021-01-02 10:25:22 +0100

    Add files via upload
    Stampa dati su file

---
+++ Energy-Aware-Service-Assembly/src/lnu/mida/entity/NetworkStatusManager.java
@@ -0,0 +1,48 @@
+package lnu.mida.entity;
+
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import lnu.mida.controller.init.OverloadFileInitializer;
+import lnu.mida.entity.GeneralNode;
+import lnu.mida.entity.Service;
+import lnu.mida.protocol.OverloadComponentAssembly;
+import peersim.cdsim.CDState;
+import peersim.config.Configuration;
+import peersim.core.Network;
+import peersim.core.Node;
+
+public class NetworkStatusManager {
+
+
+	
+	private int original_network_size;
+	
+	public NetworkStatusManager() {
+
+		original_network_size = Configuration.getInt("NETWORK_SIZE",1);
+	}
+	
+	
+	public void printStatus() {
+	
+		//System.out.println("********* dead nodes = " + dead_nodes + "     original = " +original_network_size +  " ********** ");
+
+		
+		PrintStream ps_first = OverloadFileInitializer.getPs_first();
+		PrintStream ps_last = OverloadFileInitializer.getPs_last();
+			
+		if(Network.size()==original_network_size) {
+			System.out.println("T_all");
+			//ps_first.print(CDState.getCycle()+"\n");
+		}
+		if(Network.size()==5) {
+			System.out.println("T_one");
+			//ps_last.print(CDState.getCycle()+"\n");
+		}
+				
+	}
+	
+	
+}
