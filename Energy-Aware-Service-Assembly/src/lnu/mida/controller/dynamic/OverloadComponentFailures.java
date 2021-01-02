c87d5700a13dd8e18b52d8d0c1d1ef2f2f72aad8
francescaricci10 <40519199+francescaricci10@users.noreply.github.com>
2021-01-02 10:43:31 +0100

    Update OverloadComponentFailures.java

--- Energy-Aware-Service-Assembly/src/lnu/mida/controller/dynamic/OverloadComponentFailures.java
+++ Energy-Aware-Service-Assembly/src/lnu/mida/controller/dynamic/OverloadComponentFailures.java
@@ -2,7 +2,9 @@ package lnu.mida.controller.dynamic;
 
 import java.util.ArrayList;
 
+import lnu.mida.controller.OverloadReset;
 import lnu.mida.entity.GeneralNode;
+import lnu.mida.entity.NetworkStatusManager;
 import lnu.mida.entity.Service;
 import lnu.mida.protocol.OverloadApplication;
 import lnu.mida.protocol.OverloadComponentAssembly;
@@ -68,7 +70,14 @@ public class OverloadComponentFailures implements Control {
 
 
 		for (int k = 0; k < num;k++) {
+			if(Network.size()==0)
+				return false;
 			int j = CommonState.r.nextInt(Network.size());
+			
+			remove_links(j);
+
+			NetworkStatusManager man = new NetworkStatusManager();
+			man.printStatus();
 			
 			GeneralNode node = (GeneralNode) Network.remove(j);			
 			OverloadComponentAssembly ca = (OverloadComponentAssembly) node.getProtocol(component_assembly_pid);
@@ -93,6 +102,55 @@ public class OverloadComponentFailures implements Control {
 		
 		return false;				
 		
+	}
+	
+	
+	
+	
+	public void remove_links(int node_index) {
+		
+		GeneralNode n = (GeneralNode) Network.get(node_index);		
+		OverloadComponentAssembly ca = (OverloadComponentAssembly) n.getProtocol(component_assembly_pid);		
+		
+		ArrayList<Service> services_to_del = ca.getServices();
+		for (Service service2 : services_to_del) {
+			service2.reset();
+		}				
+
+		for (int i = 0; i < Network.size(); i++) {
+			GeneralNode node_to_check = (GeneralNode) Network.get(i);		
+						
+			// si scorrono tutti i servizi
+			OverloadComponentAssembly ca_to_check = (OverloadComponentAssembly) node_to_check.getProtocol(component_assembly_pid);		
+			ArrayList<Service> services_to_check = ca_to_check.getServices();
+
+			for (Service service : services_to_check) {
+				Service[] listDepObj = service.getDependencies_obj();
+				boolean[] listDep = service.getDependencies();
+				
+				if(listDepObj==null)
+					continue;
+				
+				for (int j = 0; j < listDep.length; j++) {
+					
+					boolean dep = listDep[j];
+					if (dep == true) {
+						Service depObj = listDepObj[j];
+
+						if(depObj==null)
+							continue;
+						
+						for (Service s : services_to_del) {
+							if(depObj==s)
+								service.unlinkDependency(s);
+							
+						}
+					}
+				}
+			}
+			
+		}
+				
 	}
 
 }
