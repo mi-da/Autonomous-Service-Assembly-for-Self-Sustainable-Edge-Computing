239742d8816275618d1c24088e783c4750a5053e
Mirko <mirko.dng@gmail.com>
2022-09-29 10:26:36 +0200

    rename policies on script

--- Energy-Aware-Service-Assembly/script.R
+++ Energy-Aware-Service-Assembly/script.R
@@ -5,31 +5,31 @@ library(ggplot2)
 # set working directory here
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-overall <- read.table("exp_assembly_overall_energy_template_1652968342683.txt", 
+overall <- read.table("exp_assembly_overall_energy_template_1664433597005.txt", 
                       header = F,
                       sep = " ",
                       col.names= c("overallx", "overally")
 )
 
-random <- read.table("exp_assembly_random_1652946312658.txt", 
+random <- read.table("exp_assembly_random_1664434657959.txt", 
                      header = F,
                      sep = " ",
                      col.names= c("randomx", "randomy")
 )
 
-res <- read.table("exp_assembly_residual_life_template_1652946380725.txt", 
+res <- read.table("exp_assembly_residual_life_template_1664435107166.txt", 
                   header = F,
                   sep = " ",
                   col.names= c("resx","resy")
 )
 
-local <- read.table("exp_assembly_local_energy_template_1652950507165.txt", 
+local <- read.table("exp_assembly_local_energy_template_1664433626182.txt", 
                     header = F,
                     sep = " ",
                     col.names= c("locx","locy")
 )
 
-energyAware <- read.table("exp_assembly_energyAware_1652950789392.txt", 
+learning <- read.table("exp_assembly_energyAware_1664434759956.txt", 
                           header = F,
                           sep = " ",
                           col.names= c("enx","eny")
@@ -42,11 +42,11 @@ overall_y <- overall$overally
 random_y <- random$randomy
 res_y <- res$resy
 local_y <- local$locy
-energyAware_y <- energyAware$eny
+energyAware_y <- learning$eny
 
 head(overall)
 
-colors <- c("overall" = "#EEA236", "residual life" = "#5CB85C", "random" = "#46B8DA", "local"="gray64", "energyAware"="deeppink3")
+colors <- c("overall" = "#EEA236", "residual life" = "#5CB85C", "random" = "#46B8DA", "local"="gray64", "learning"="deeppink3")
 
 ggplot(data=local, aes(x=overall_x, col = group)) + 
   
@@ -54,7 +54,7 @@ ggplot(data=local, aes(x=overall_x, col = group)) +
   geom_line(aes(y = res_y, color = "residual life"), size = 1) +
   geom_line(aes(y = random_y, color = "random"), size = 1) +
   geom_line(aes(y = local_y, color = "local"), size = 1) +
-  geom_line(aes(y = energyAware_y, color = "energyAware"), size = 1) +
+  geom_line(aes(y = energyAware_y, color = "learning"), size = 1) +
   
   labs(x = "round",
        y = "nodes availability",
